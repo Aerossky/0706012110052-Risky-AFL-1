757023e0405a2ec9d857634c7c9b2b9702481337
Aerossky <riskyradeonrx560@gmail.com>
2023-04-06 08:36:43 +0700

    finish without bug check

---
+++ 0706012110052-Risky-AFL 1/Protocols/ItemProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  ItemProtocol.swift
+//  0706012110052-Risky-AFL 1
+//
+//  Created by MacBook Pro on 05/04/23.
+//
+
+import Foundation
---
+++ 0706012110052-Risky-AFL 1/Protocols/MenuProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  MenuProtocol.swift
+//  0706012110052-Risky-AFL 1
+//
+//  Created by MacBook Pro on 05/04/23.
+//
+
+import Foundation
